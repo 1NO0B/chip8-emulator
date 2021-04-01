f1667681cc0ba9172089a760208acf9ef946c071
1NO0B <noob@users.noreply.github.com>
2021-04-01 20:46:23 +0200

    finished
    don't know if it works yet bc windows is trash

--- chip8.cpp
+++ chip8.cpp
@@ -14,7 +14,7 @@ License: "MIT License"
 #include<iostream>
 #include<cstdint>
 #include<fstream>
-
+#include <chrono>
 #include <SDL2/SDL.h>
 
 
@@ -22,10 +22,13 @@ using namespace std;
 
 //rand() % 255;
 
-const string filename = "filename";
+char const* filename = "PONG";
 
 const unsigned int start_address = 0x200;
 const unsigned int fontset_start_address = 0x50;
+const unsigned int VIDEO_HEIGHT = 32;
+const unsigned int VIDEO_WIDTH = 64;
+
 
 
 
@@ -85,7 +88,7 @@ public:
 	    	for (long i = 0; i < size; ++i){
 		    	memory[start_address + i] = buffer[i];
 	    	}
-
+            
 	    	delete[] buffer;
 	    }
     }
@@ -232,12 +235,12 @@ void op_Dxyn(){ // Display n-byte sprite starting at memory location I at (Vx, V
 }
 
 void op_Ex9e(){ // Skip next instruction if key with the value of Vx is pressed
-    if(keys[registers[(opcode & 0x0F00)>>8]] != 1){
+    if(keys[registers[(opcode & 0x0F00)>>8]] == true){
         pc+=2;
     }
 }
 void op_ExA1(){ // Skip next instruction if key with the value of Vx is not pressed
-    if(keys[registers[(opcode & 0x0F00)>>8]] == 1){
+    if(keys[registers[(opcode & 0x0F00)>>8]] == false){
         pc+=2;
     }
 }
@@ -247,7 +250,7 @@ void op_Fx07(){ // Set Vx = delay timer value
 void op_Fx0A(){ // Wait for a key press, store the value of the key in Vx
     bool keypressed = false;
     for (int i = 0; i<15; i++){   
-        if(keys[i]!=0){
+        if(keys[i] == true){
             registers[(opcode & 0x0F00)>>8] = keys[i];
             keypressed = true;
         }
@@ -402,15 +405,265 @@ void cycle(){ //execudes one cycle of the cpu
 
 class Setup{ //the class which represents your setup -> your monitor, your keypad etc.
 
+public:
+    SDL_Window* window{};
+	SDL_Renderer* renderer{};
+	SDL_Texture* texture{};
+
+    Setup(char const* title, int window_width, int window_height, int texture_width, int texture_height){ //builds the window
+
+        SDL_Init(SDL_INIT_VIDEO);
+        window = SDL_CreateWindow(title, 0, 0, window_width, window_height, SDL_WINDOW_SHOWN);
+
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+        texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, texture_width, texture_height);
+    }
+
+    ~Setup(){ //closes the window
+        SDL_DestroyTexture(texture);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+
+
+    void update(void const* buffer, int pitch){
+        SDL_UpdateTexture(texture, nullptr, buffer, pitch);
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+		SDL_RenderPresent(renderer);
+    }
+
+
+    bool process_input(uint8_t* keys){
+        bool quit = false;
+
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type){
+				case SDL_QUIT:
+				{
+					quit = true;
+				} break;
+
+				case SDL_KEYDOWN:
+				{
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_ESCAPE:
+						{
+							quit = true;
+						} break;
+
+						case SDLK_x:
+						{
+							keys[0] = 1;
+						} break;
+
+						case SDLK_1:
+						{
+							keys[1] = 1;
+						} break;
+
+						case SDLK_2:
+						{
+							keys[2] = 1;
+						} break;
+
+						case SDLK_3:
+						{
+							keys[3] = 1;
+						} break;
+
+						case SDLK_q:
+						{
+							keys[4] = 1;
+						} break;
+
+						case SDLK_w:
+						{
+							keys[5] = 1;
+						} break;
+
+						case SDLK_e:
+						{
+							keys[6] = 1;
+						} break;
+
+						case SDLK_a:
+						{
+							keys[7] = 1;
+						} break;
+
+						case SDLK_s:
+						{
+							keys[8] = 1;
+						} break;
+
+						case SDLK_d:
+						{
+							keys[9] = 1;
+						} break;
+
+						case SDLK_z:
+						{
+							keys[0xA] = 1;
+						} break;
+
+						case SDLK_c:
+						{
+							keys[0xB] = 1;
+						} break;
+
+						case SDLK_4:
+						{
+							keys[0xC] = 1;
+						} break;
+
+						case SDLK_r:
+						{
+							keys[0xD] = 1;
+						} break;
+
+						case SDLK_f:
+						{
+							keys[0xE] = 1;
+						} break;
+
+						case SDLK_v:
+						{
+							keys[0xF] = 1;
+						} break;
+					}
+				} break;
+
+				case SDL_KEYUP:
+				{
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_x:
+						{
+							keys[0] = 0;
+						} break;
+
+						case SDLK_1:
+						{
+							keys[1] = 0;
+						} break;
+
+						case SDLK_2:
+						{
+							keys[2] = 0;
+						} break;
+
+						case SDLK_3:
+						{
+							keys[3] = 0;
+						} break;
+
+						case SDLK_q:
+						{
+							keys[4] = 0;
+						} break;
+
+						case SDLK_w:
+						{
+							keys[5] = 0;
+						} break;
+
+						case SDLK_e:
+						{
+							keys[6] = 0;
+						} break;
+
+						case SDLK_a:
+						{
+							keys[7] = 0;
+						} break;
+
+						case SDLK_s:
+						{
+							keys[8] = 0;
+						} break;
+
+						case SDLK_d:
+						{
+							keys[9] = 0;
+						} break;
+
+						case SDLK_z:
+						{
+							keys[0xA] = 0;
+						} break;
+
+						case SDLK_c:
+						{
+							keys[0xB] = 0;
+						} break;
+
+						case SDLK_4:
+						{
+							keys[0xC] = 0;
+						} break;
+
+						case SDLK_r:
+						{
+							keys[0xD] = 0;
+						} break;
+
+						case SDLK_f:
+						{
+							keys[0xE] = 0;
+						} break;
+
+						case SDLK_v:
+						{
+							keys[0xF] = 0;
+						} break;
+					}
+				} break;
+            }
+
+        }
+        return quit;
+    }
 };
 
 
 int main() { 
-    Chip8 chip8;
     
-    chip8.start(); //called at the "boot" of the chip
+	int cycleDelay = 1200;
+	Setup setup("CHIP-8 Emulator", VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_WIDTH, VIDEO_HEIGHT);
+	Chip8 chip8;
+	chip8.start();
     
-    return 0;
+	int videoPitch = sizeof(chip8.display_memory[0]) * VIDEO_WIDTH;
+    
+	auto lastCycleTime = std::chrono::high_resolution_clock::now();
+	bool quit = false;
+
+	while (!quit)
+	{
+		quit = setup.process_input(chip8.keys);
+
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+
+		if (dt > cycleDelay)
+		{
+			lastCycleTime = currentTime;
+
+			chip8.cycle();
+        
+			setup.update(chip8.display_memory, videoPitch);
+		}
+        
+	}
+
+	return 0;
 }
 
 
